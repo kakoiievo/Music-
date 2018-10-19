c9f0d5a0d9d429f4b66aa37d29a40dea48cb34af
perkinsung <perkinsung@310-40.local>
2018-10-19 09:16:18 +0800

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by perkinsung on 2018/10/19.
+//  Copyright © 2018 perkinsung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
